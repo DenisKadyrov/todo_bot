336e382966a7814767bc2de0a886f6b42b685e52
Denis <kripmrx@gmail.com>
2024-11-11 19:05:42 +0100

    add configs

---
+++ config.py
@@ -0,0 +1,37 @@
+from typing import Any, Dict, Optional
+
+from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn, validator, AnyHttpUrl
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_PORT: int
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+    
+    SITE_URL: AnyHttpUrl
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()
